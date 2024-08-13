40d3f07c44a762834f9dd981b9048b6282f78286
Ana Beatriz <anabeatrizdscotrim@gmail.com>
2024-08-13 17:01:29 -0300

    Atualizações no projeto Segunda Aula

---
+++ Models/Endereco.js
@@ -0,0 +1,56 @@
+const {Model, DataTypes} = require('sequelize');
+
+class Endereco extends Model {}
+
+Endereco.init({
+    Id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+
+    Cep: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    Logradouro: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    Numero: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    Complemento: {
+        type: DataTypes.STRING,
+    },
+    Bairro:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    Cidade: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    Estado: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    MunicipioIBGE:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+}, {
+    sequelize,
+    modelName:'Endereco',
+    tableName: 'enderecos',
+    timestamps: true,
+
+});
+
+model.exports = Endereco;
